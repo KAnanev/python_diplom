030c65f1a6541f49aac5bc55241f98e8ceaeb753
KAnanev <russrage@gmail.com>
2020-03-25 18:26:32 +0400

    next

--- find_groups_user.py
+++ find_groups_user.py
@@ -6,6 +6,7 @@ GET_URL = 'https://api.vk.com/method/'
 user = 'eshmargunov'
 list_users = []
 
+
 class User:
 
     def __init__(self, user_id):
@@ -24,31 +25,32 @@ class User:
     def __str__(self):
         return str(self.user_id)
 
-    def get_user_groups(self):
-        response = requests.get(
-            'https://api.vk.com/method/groups.get',
-            params={
-                'access_token': API_TOKEN,
-                'user_id': self.user_id,
-                'v': '5.103'
-            })
-        return response.json()
+    def get_only_my_groups(self):
+        response = requests.post(f'{GET_URL}/execute',
+                                 params={
+                                     'code': 'return [API.groups.get({"user_id": "' + str(self.user_id) + '"}).items, '
+                                                                                                          'API.groups.get({"user_id": API.friends.get'
+                                                                                                          '({"user_id": "' + str(
+                                         self.user_id) + '"})@.items}).items];',
+                                     'access_token': API_TOKEN,
+                                     'v': '5.103'
+                                 })
+        user_groups = set(response.json()['response'][0])
+        friends_groups = set(response.json()['response'][1])
+        return user_groups.difference(friends_groups)
 
-    def get_user_friends(self):
-        response = requests.get(
-            'https://api.vk.com/method/friends.get',
-            params={
-                'access_token': API_TOKEN,
-                'user_id': self.user_id,
-                'v': '5.103'
-            })
-        for i in response.json()['response']['items']:
-            user = User(i)
-            list_users.append(user)
+    def get_group_info(self, id_group):
+        if not isinstance(id_group, list):
+            id_group = ', '.join(str(i) for i in list(id_group))
+            print(id_group)
+        # for i in id_group:
+        #     response = requests.post(f'{GET_URL}/execute',
+        #                              params={
+        #                                  'code': 'return [API.groups.get().items;',
+        #                                  'access_token': API_TOKEN,
+        #                                  'v': '5.103'
+        #                              })
 
 
 obj_user = User(user)
-obj_user.get_user_friends()
-
-for i in list_users:
-    print(i.get_user_groups())
+obj_user.get_group_info(obj_user.get_only_my_groups())
